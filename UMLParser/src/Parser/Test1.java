e415be62e327f5c0b2fe8210e5a145dbdfb6492e
DESKTOP-UHTMKAQ\Elaine Ma <xm3406uscn@gmail.com>
2017-04-08 22:16:02 -0700

    edit some code

--- UMLParser/src/Parser/Test1.java
+++ UMLParser/src/Parser/Test1.java
@@ -30,33 +30,6 @@ public class Test1 {
     	
     }
  
-    // read each line of a file
-    /*
-    private String getFile(String fileName) {
-
-    	StringBuilder result = new StringBuilder("");
-
-    	//Get file from resources folder
-    	ClassLoader classLoader = getClass().getClassLoader();
-    	File file = new File(classLoader.getResource(fileName).getFile());
-
-    	try (Scanner scanner = new Scanner(file)) {
-
-    		while (scanner.hasNextLine()) {
-    			String line = scanner.nextLine();
-    			result.append(line).append("\n");
-    		}
-
-    		scanner.close();
-
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    	}
-
-    	return result.toString();
-
-      }
-      */
     
     private static File[] getFileList(String dirPath) {
         File dir = new File(dirPath);   
