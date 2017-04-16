9381be256eaa2e92c550fbd33d81a226aaa9b449
DESKTOP-UHTMKAQ\Elaine Ma <xm3406uscn@gmail.com>
2017-04-15 22:13:49 -0700

    change the main method

--- UMLParser/src/Parser/Test1.java
+++ UMLParser/src/Parser/Test1.java
@@ -11,34 +11,26 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseException;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-
+/*
+ * 
+ */
 
 
 public class Test1 {
+	private static String folderPath = "";
+	private static String fileName = "";
+	
     public static void main(String[] args) throws ParseException, IOException {
+    	//take parameters
+    	if(args.length == 2){
+    		folderPath = args[0];
+    		fileName = args[1];
+    	}else if(args.length == 0){
+    		System.out.println(" Entered 0 parameters");
+    	}
+    	
 
-        File[] fileList = getFileList("..\\UMLParser\\src\\uml_parser_test1");
-        //A a = new A();
-        for(File file : fileList) {
-            //System.out.println(file.getName());
-            CompilationUnit compilationUnit = JavaParser.parse(file);
-            System.out.println(compilationUnit.toString());
-        	//Optional<ClassOrInterfaceDeclaration> classA = compilationUnit.getClassByName(compilationUnit);
-        }
-        //Test1 obj = new Test1();
-    	System.out.println();
     	
     }
  
-    
-    private static File[] getFileList(String dirPath) {
-        File dir = new File(dirPath);   
-
-        File[] fileList = dir.listFiles(new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                return name.endsWith(".java");
-            }
-        });
-        return fileList;
-    }
 }
